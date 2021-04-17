3a74eaf8d72861c0d03057ed7a39572c4ae1aa52
= <=>
2021-04-17 13:31:31 +0530

    changes on Apr 17 2021

--- src/styledButton.js
+++ src/styledButton.js
@@ -1,12 +1,26 @@
-import styled from 'styled-components'
+//import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 
 // Create a Title component that'll render an <h1> tag with some styles
-const SButton = styled.button`
-  font-size: 1.5em;
-  background-color: yellow;
-  color: palevioletred;
-`;
+const SButton = styled.a`
+/* This renders the buttons above... Edit me! */
+display: inline-block;
+border-radius: 3px;
+padding: 0.5rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: skyblue;
+color: white;
+border: 2px solid white;
+
+/* The GitHub button is a primary button
+ * edit this to target it specifically! */
+${props => props.primary && css`
+  background: white;
+  color: black;
+`}
+`
 
 function StyledButton(msg) {
     return (
